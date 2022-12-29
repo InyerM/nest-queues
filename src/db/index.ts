5b144be75b9083104d25a1f7d788fad501f12b12
Inyer Marín <inyermedina10@gmail.com>
2022-12-29 15:46:07 -0500

    ADD : implements .env variables to connect to db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,24 @@
 import { DataSource } from 'typeorm' 
 
 import { CronJob } from './entities'
+import * as dotenv from 'dotenv'
 
-/* Creating a new DataSource object. */
+dotenv.config()
+
+/**
+ *  Creating a new DataSource object loaded with 
+ * .env variables. Check the .env.example file for more details. 
+ * */ 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "1111",
-  database: "postgres",
+  host: process.env.PG_HOST || "localhost",
+  port: Number(process.env.PG_PORT) || 5432,
+  username: process.env.PG_USER || "postgres",
+  password: process.env.PG_PASSWORD || "postgres",
+  database: process.env.PG_DATABASE || "postgres",
   entities: [CronJob],
   synchronize: true,
-  logging: true
+  logging: false
 })
 
 export {
